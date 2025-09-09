017d64341afa2bf1607eba8da6b5e455efb926e3
DarshanaMalewar25C-DAC <darshana.malewar.cmaug25@gmail.com>
2025-09-09 20:15:00 +0530

    Add files via upload
    Assignment 2 of LB

---
+++ Assignment 2/Weekdays.java
@@ -0,0 +1,31 @@
+class Weekdays{
+	public static void main(String args[]){
+		int day = 9;
+		switch(day){
+			case 1:
+			System.out.println("The day is Monday.");
+			break;
+			case 2:
+			System.out.println("The day is Tuesday.");
+			break;
+			case 3:
+			System.out.println("The day is Wednesday.");
+			break;
+			case 4:
+			System.out.println("The day is Thursday.");
+			break;
+			case 5:
+			System.out.println("The day is Friday.");
+			break;
+			case 6:
+			System.out.println("The day isSaturday.");
+			break;
+			case 7:
+			System.out.println("The day isSunday.");
+			break;
+			default:
+			System.out.println("Invalid day number.");
+
+		}
+	}
+}
